d8e532bbab97c61ea91597d9ad503df5fad9034d
Ben Wojtowicz <Bwojtowicz4483@stu.d214.org>
2023-09-21 10:10:11 -0500

    Sending Headerview

--- ContentView.swift
+++ ContentView.swift
@@ -2,10 +2,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        var array = ["Mow the Lawn", "Feed the Dog", "Grocery Shopping"]
+        var array: [Task] = [Task(nameOfTask: "")]
         List{
-            ForEach(0 ..< array.count, id: \.self) {value in
-                Text(array[value])
+            ForEach(0 ..< array.count, id: \.self) {value in Task(nameOfTask: "")
+                
             }
         }
     }
--- Header View.swift
+++ Header View.swift
@@ -1,11 +1,8 @@
 //
-//  Task.swift
+//  Header View.swift
 //  GitHubPractice1
 //
 //  Created by Ben Wojtowicz on 9/28/23.
 //
 
 import Foundation
-struct Task: Hashable {
-    var nameOfTask = ""
-}
