33fc88b95cad52e03a2ec2f23f96bc2dbc8d6fa9
Ben Wojtowicz <Bwojtowicz4483@stu.d214.org>
2023-09-28 10:14:50 -0500

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- Header View.swift
+++ Header View.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct HeaderView: View {
+    @Binding var tasks:[Task];
+    @State var name = ""
+    var body: some View {
+        HStack{
+            TextField("Tassk", text: $name )
+            
+
+        }
+    }
+}
